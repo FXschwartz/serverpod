881acd901e493a139c3a0dc92cc437d0b26506a2
Preston <preston.schwartz505@gmail.com>
2026-03-24 17:46:50 -0500

    Rebased and added missing migration.

---
+++ examples/auth/auth_server/migrations/20260324224322593/migration.sql
@@ -0,0 +1,53 @@
+BEGIN;
+
+--
+-- ACTION CREATE TABLE
+--
+CREATE TABLE "serverpod_reactive_db_call" (
+    "id" bigserial PRIMARY KEY,
+    "handlerName" text NOT NULL,
+    "sourceTable" text NOT NULL,
+    "operation" text NOT NULL,
+    "rowData" text NOT NULL,
+    "createdAt" timestamp without time zone NOT NULL
+);
+
+-- Indexes
+CREATE INDEX "serverpod_reactive_db_call_created_at_idx" ON "serverpod_reactive_db_call" USING btree ("createdAt");
+CREATE INDEX "serverpod_reactive_db_call_handler_name_idx" ON "serverpod_reactive_db_call" USING btree ("handlerName");
+
+
+--
+-- MIGRATION VERSION FOR auth
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('auth', '20260324224322593', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260324224322593', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20260323224834904', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260323224834904', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_auth_idp
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth_idp', '20260213194423028', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260213194423028', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod_auth_core
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod_auth_core', '20260129181112269', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260129181112269', "timestamp" = now();
+
+
+COMMIT;
